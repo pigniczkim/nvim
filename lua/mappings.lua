b932c70acf2a5c9bbc3142e487bbf07a5918f797
pigniczkim <m.pigniczki@hawe.de>
2024-12-12 16:40:21 +0100

    symbols with telescope

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,9 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Telescope mappings
+local builtin = require("telescope.builtin")
+map('n', '<leader>ls', builtin.lsp_document_symbols)
+map('n', '<leader>ws', builtin.lsp_dynamic_workspace_symbols)
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
