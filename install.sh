b76952f69199c745c7f2a47c1a614bf229d6c33f
Pigniczki <m.pigniczki@hawe.de>
2025-03-14 03:33:48 +0100

    update neovim install

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
 
 sudo apt update && sudo apt install -y \
-	neovim \
-	ripgrep
+	ripgrep \
+	unzip
 
+# Install the latest neovim
+curl -L https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz | tar -xz --strip-components=1 -C /usr nvim-linux-x86_64
+
+# TODO: Install node
+#curl -o- https://fnm.vercel.app/install | bash
+#fnm install 22
+
+# Clone the base configuration
 git clone --depth 1 https://github.com/pigniczkim/nvim.git ~/.config/nvim
 
 nvim
